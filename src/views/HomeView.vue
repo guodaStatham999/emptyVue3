c39f772dbc0802277abdfd09498bfa67be3c025d
tangkaifang <kaifang.tang@wenge.com>
2023-02-09 17:40:21 +0800

    小图片没问题了

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,8 +31,8 @@ export default {
       // 视频
       // state.baseOptions.url = 'https://tilake.wenge.com/wos/data-center-a3f5de5e3/5ed6f010c8ab7606274901c13d4a6ec0.mp4'
       // 图片
-      // state.baseOptions.url = 'https://tse1-mm.cn.bing.net/th/id/OIP-C.QPH1IBosDYBqaU3O6wV3YAHaEo?pid=ImgDet&rs=1' // 小图片
-      state.baseOptions.url = 'https://ts1.cn.mm.bing.net/th/id/R-C.909139736b8bee17b50707506f7f803c?rik=PPGA9Jzj8BtLiQ&riu=http%3a%2f%2fuploadfile.bizhizu.cn%2f2015%2f1221%2f20151221022310156.jpg&ehk=jYE8M%2fmDiA4z%2bHbJs1crsA5i%2f7L577w2ZcPor%2bs0AqI%3d&risl=&pid=ImgRaw&r=0'
+      state.baseOptions.url = 'https://tse1-mm.cn.bing.net/th/id/OIP-C.QPH1IBosDYBqaU3O6wV3YAHaEo?pid=ImgDet&rs=1' // 小图片
+      // state.baseOptions.url = 'https://pic3.zhimg.com/v2-c6ae9c3aff36b9b221258f6a90577902_r.jpg' // 大图片
     },1000)
     return {
       ...toRefs(state),
--- src/views/mark copy.vue
+++ src/views/mark copy.vue
@@ -1,20 +1,14 @@
 
   <template>
-  <div
-    ref="chartWrapRef"
-    id="chartWrapId"
-    class="chartWrap"
-    :dimension="dimension"
-  >
-    <svg id="svgId" :width="dimension.svg.width" :height="dimension.svg.height">
-      <image
-        :width="dimension.svg.width"
-        :height="dimension.svg.height"
-        :href="url"
-        class="control-img"
-      />
-    </svg>
-  </div>
+    <div>
+      <!-- :style="dimension.marginStyle" -->
+      <!-- <img :src="url" alt=""> -->
+      <div id="chartWrapId" class="chartWrap"  >
+        <svg :width="width" :height="height">
+          <image :width="width" :height="height" :href="url" class="control-img"  />
+        </svg>
+      </div>
+    </div>
   <!-- <button @click="create">创建</button>
   <button @click="move">移动</button> -->
 </template>
@@ -30,34 +24,26 @@ export default {
   data() {
     return {
       // 图片宽高,是否加载完成,地址
-      loaded: false,
+      loaded:false,
       width: 0,
       height: 0,
       url: this.baseOptions.url,
-      bounding: null,
+      bounding:null,
       isDrag: false,
       rectData: [],
       start: false,
       startDom: "",
       Drag: null,
 
+
+
       list: [
-        {
-          endX: 50,
-          endY: 50,
-          height: 50,
-          name: "rect-g-1675329600098",
-          preId: "1675329600098",
-          startX: 0,
-          startY: 0,
-          width: 50,
-        },
         {
           endX: 334,
           endY: 156,
           height: 54,
-          name: "rect-g-1675329600099",
-          preId: "1675329600099",
+          name: "rect-g-1675329600098",
+          preId: "1675329600098",
           startX: 138,
           startY: 102,
           width: 196,
@@ -78,23 +64,21 @@ export default {
       };
       // 这里需要继续计算边框距离和图片大小后计算一个缩放值;
       // 当 dom 元素加载完毕来计算 svg 尺寸
-      if (
-        !this.isNil(this.bounding) &&
-        !!this.loaded &&
-        this.$refs.chartWrapRef
-      ) {
-        let imgInfo = {
+      console.log(!this.isNil(this.bounding))
+      console.log(this.loaded);
+      console.log(!!this.loaded);
+      if (!this.isNil(this.bounding) && !!this.loaded ) {
+        let imgInfo = { 
           width: this.width,
-          height: this.height,
-        };
+          height:  this.height
+        }
         // 容器的最大宽度、高度
         const { width: cw, height: ch, left, top } = this.bounding;
         // 图片的宽度，高度
         const iw = imgInfo.width;
         const ih = imgInfo.height;
 
-        const wrapperDimension = {
-          // 容器宽高,距离左边的值
+        const wrapperDimension = { // 容器宽高,距离左边的值
           width: cw,
           height: ch,
           left,
@@ -111,7 +95,7 @@ export default {
         // 如果图片有缩放，直接取容器尺寸即可
         const svgDimension = {
           width: imgScale < 1 ? cw : Math.min(iw, cw),
-          height: imgScale < 1 ? ch : Math.min(ih, ch),
+          height: imgScale < 1 ? ch  : Math.min(ih, ch),
         };
 
         // 标注相关元素的容器
@@ -173,7 +157,8 @@ export default {
           wrapper: wrapperDimension, // 暂时没找到用户
           margin, // 就是偏移结果,只不过是不带单位的
         };
-        return res;
+        console.log(res);
+        return res
       }
       return defaultDimension;
     },
@@ -182,13 +167,18 @@ export default {
   watch: {
     "baseOptions.url": {
       handler(newVal, oldVal) {
+
+
         let img = new Image();
         img.onload = () => {
-          this.loaded = true;
+
+          this.loaded = true
           this.width = img.width + "";
           this.height = img.height + "";
           this.url = newVal;
           this.init();
+          this.bounding = this.getBounding(document.getElementById('chartWrapId'))
+
         };
         img.src = newVal;
       },
@@ -197,19 +187,32 @@ export default {
     list: {
       handler(newVal, oldVal) {
         this.init();
+        // newVal.forEach(item=>this.mountedList(item))
       },
       deep: true,
     },
   },
-  mounted() {
-    this.bounding = this.getBounding(document.getElementById("chartWrapId"));
-  },
   methods: {
+    create() {
+      this.isDrag = false;
+    },
+    move() {
+      this.isDrag = true;
+    },
     init() {
-      D3.select("#rect-g").remove();
+      // document.getElementById("chartWrapId").innerHTML = "";
       // return
-      const svg = D3.select("#svgId");
+      const svg = D3.select("#chartWrapId")
+        // .append("svg")
+        // .attr("width", this.width)
+        // .attr("height", this.height);
 
+      // svg
+      //   .append("image")
+      //   .attr("class", "control-img")
+      //   .attr("height", this.height)
+      //   .attr("width", this.width)
+      //   .attr("href", this.url);
       svg.on("mouseleave", this.mouseLeaveInit); // 鼠标离开svg触发
       svg.on("mouseenter", () => {
         this.createCrossPoint(svg);
@@ -257,7 +260,6 @@ export default {
         .attr("r", 5)
         .attr("fill", "red"); */
     },
-    // 把删除逻辑拆出来,梳理下每次添加/修改位置的时候,如何增加 偏移值
     mountedList(item) {
       let that = this;
       D3.select("#rect-g")
@@ -310,13 +312,9 @@ export default {
                 that.Drag(D3.select(this)); // 将圆添加到拖拽方法中
               });
             that.Drag(D3.select(this)); // 将矩形添加到拖拽方法中
-          } else {
-            // 不是拖拽情况下,也要添加4个圆
           }
         })
         .on("mouseout", function (e) {
-          // 没啥大用,但是就感觉开通以后4个点正常了
-          e.stopPropagation();
           let picRelationLeft = e.offsetX;
           let picRelationTop = e.offsetY;
           let isIn = that.isInMarkList(picRelationLeft, picRelationTop);
@@ -345,6 +343,7 @@ export default {
             widget.attr("fill", color);
             widget = null; // 被拖拽的元素设为空
 
+            console.log(111, "删除");
             D3.select(`#rect-g-${item.preId}`).selectAll("circle").remove(); // 删除四个球
           }
         });
@@ -353,6 +352,7 @@ export default {
         +D3.select(`#rect-g-${item.preId}-rect`).attr("x"),
         +D3.select(`#rect-g-${item.preId}-rect`).attr("y"),
       ]; // 顶点坐标
+      console.log(xy);
       const wh = [
         +D3.select(`#rect-g-${item.preId}-rect`).attr("width"),
         +D3.select(`#rect-g-${item.preId}-rect`).attr("height"),
@@ -406,19 +406,15 @@ export default {
       // .attr('height',`${wh[1]}`)
 
       delCon.on("click", function (e) {
-        e.stopPropagation();
-
+        console.log(e);
         const xy = [
           +D3.select(`#rect-g-${item.preId}-rect`).attr("x"),
           +D3.select(`#rect-g-${item.preId}-rect`).attr("y"),
         ];
+        console.log(xy);
         alert(
           `发送请求,组件往出派发事件即可,xy点位点: ${xy},宽高: ${wh},id: ${item.preId}`
         );
-        that.list.filter((l) => {
-          console.log(l);
-          return l.preId !== item.preId;
-        });
       });
     },
 
@@ -433,7 +429,6 @@ export default {
     },
 
     isInMarkList(x, y) {
-      // 已经加上了左侧和上侧的偏移量
       let res = this.list.filter((item) => {
         return this.IsPointInMatrix(
           { x: item.startX, y: item.startY },
@@ -445,18 +440,7 @@ export default {
             y,
           }
         );
-        // return this.IsPointInMatrix(
-        //   { x: item.startX + this.dimension.margin.left, y: item.startY  + this.dimension.margin.top},
-        //   { x: item.endX + this.dimension.margin.left, y: item.startY+ this.dimension.margin.top },
-        //   { x: item.endX + this.dimension.margin.left, y: item.endY + this.dimension.margin.top},
-        //   { x: item.startX + this.dimension.margin.left, y: item.endY + this.dimension.margin.top},
-        //   {
-        //     x,
-        //     y,
-        //   }
-        // );
       });
-      // console.log(x,y,'xy');
       // console.log(!!(res && res.length));
       return !!(res && res.length);
     },
@@ -468,9 +452,6 @@ export default {
       this.isDrag = isIn;
 
       const that = this;
-      // 想下是不是所有修改点的位置都要根据缩放值来计算
-      // 点下,移动,抬起.
-      // 整块拖拽,四个点拖拽,删除按钮位置
       if (!this.isDrag) {
         // 不是在拖拽模式
         let id = new Date().getTime() + ""; // 获取一个不重复的id,给矩形分配一个id
@@ -571,12 +552,11 @@ export default {
       this.isDrag = isIn;
 
       //   初始化触发移动事件的部分逻辑,直接把线画出来. 或者在初始化的时候把这些逻辑写出来
+      let that = this;
       if (!this.isDrag) {
         //判断不是在拖拽状态下
         const xy = [+e.offsetX, +e.offsetY]; // 获取移动的鼠标的坐标点
-
-        /* 
-      // 先处理十字坐标，圆点和两条线 -- 已经关闭   
+        // 先处理十字坐标，圆点和两条线
         D3.select("#line-x")
           .attr("x1", 0)
           .attr("y1", xy[1])
@@ -595,8 +575,7 @@ export default {
           .attr("cx", xy[0])
           .attr("r", 6)
           .attr("cy", xy[1])
-          .attr("fill", "red"); 
-          */
+          .attr("fill", "red");
         if (this.start) {
           // 正在添加矩形（mousedown把start设为true）
           let top; // 判断当前矩形的顶点坐标（矩形是由顶点，宽高定义的）
@@ -623,11 +602,13 @@ export default {
             .attr("y", top[1])
             .attr("width", Math.abs(this.rectData[0] - xy[0]))
             .attr("height", Math.abs(xy[1] - this.rectData[1]));
+        } else {
         }
+      } else {
       }
     },
     mouseLeaveInit(e) {
-      // D3.select("#line-g").remove();
+      D3.select("#line-g").remove();
       this.mouseUp(e); // 离开触发一次抬起事件
     },
     mouseUp(e) {
@@ -644,14 +625,15 @@ export default {
           let height = +D3.select(`#${this.startDom}`)
             .style("height")
             .split("px")[0];
+          console.log(this.startDom);
           this.list.push({
             // 新增
             name: this.startDom,
             preId: this.startDom.replace(/\D/g, "").replace("-", ""),
-            startX: this.rectData[0] - this.dimension.margin.left,
-            startY: this.rectData[1] - this.dimension.margin.top,
-            endX: this.rectData[0] + width - this.dimension.margin.left,
-            endY: this.rectData[1] + height - this.dimension.margin.top,
+            startX: this.rectData[0],
+            startY: this.rectData[1],
+            endX: this.rectData[0] + width,
+            endY: this.rectData[1] + height,
             width: width,
             height: height,
           });
@@ -675,6 +657,15 @@ export default {
         endX2: xy[0] + wh[0] - 20,
         endY2: xy[1] + 20,
       };
+      // console.log(xy,'xy定位点')
+      // console.log(wh,'wh宽高')
+      // console.log(startX, startY,'第一个点左上角')
+      // console.log(endX, endY,'第一个点右下角')
+      // console.log(startX2, startY2,'第二个点左上角')
+      // console.log(endX2, endY2,'第二个点右下角')
+
+      console.log(that);
+      console.log(that.id);
       if (that.id === "111") {
         D3.select(that)
           .attr("x1", `${startX}`)
@@ -702,9 +693,8 @@ export default {
 
           if (widget._groups[0][0].localName === "rect") {
             // 当拖拽对象为矩形
-            console.log(dot[0] - +widget.attr("x"));
             widget
-              .attr("o-x", dot[0] - +widget.attr("x")) // 存储点击点到真正的x/y的间距.
+              .attr("o-x", dot[0] - +widget.attr("x"))
               .attr("o-y", dot[1] - +widget.attr("y"));
             D3.select(`#${id}`)
               .selectAll("circle")
@@ -756,10 +746,10 @@ export default {
           if (widget._groups[0][0].localName === "rect") {
             // 拖拽对象为矩形
             const id = widget._groups[0][0].parentNode.id; // 获取父元素的id
-
-            // o-x 只有这里和拖拽点用到了,还是用来更新4个黄色点的
+            // consle.log
+            // debugger
             widget
-              .attr("x", dot[0] - +widget.attr("o-x")) // 减去前面存储的间距,就是左上角
+              .attr("x", dot[0] - +widget.attr("o-x"))
               .attr("y", dot[1] - +widget.attr("o-y")); // 更新矩形的顶点信息
             D3.select(`#${id}`)
               .selectAll("circle")
@@ -770,6 +760,7 @@ export default {
               });
 
             const xy = [Number(widget.attr("x")), Number(widget.attr("y"))]; // 顶点坐标
+            console.log(xy);
             const wh = [
               Number(D3.select(this).attr("width")),
               Number(D3.select(this).attr("height")),
@@ -843,6 +834,7 @@ export default {
 
             // 圆拖拽部分,处理x
             const xy = [+top[0], +top[1]]; // 顶点坐标
+            console.log(xy);
             const wh = [
               Math.abs(that.topDot[0] - dot[0]), // width
               Math.abs(dot[1] - that.topDot[1]), // height
@@ -860,50 +852,94 @@ export default {
                 }
               });
           }
+
+          /*        // 不管什么拖拽都需要修改x的位置
+        const xy = [
+          +dot[0],
+          +dot[1],
+        ]; // 顶点坐标
+
+        const wh = [
+          Number(D3.select(this).attr('width')),
+          Number(D3.select(this).attr('height'))
+        ]; // 矩形宽高
+
+
+        const { startX, endX, startY, endY, startX2,endX2, startY2 , endY2} = {
+          startX: xy[0] + wh[0] - 20,
+          startY: xy[1] + 10,
+          endX: xy[0] + wh[0] - 10,
+          endY: xy[1] + 20,
+
+          startX2: xy[0] + wh[0] - 10,
+          startY2: xy[1] + 10,
+          endX2: xy[0] + wh[0] - 20,
+          endY2: xy[1] + 20,
+        };
+        // console.log(xy);
+        // console.log(wh);
+        // console.log(startX, endX, startY, endY, startX2,endX2, startY2 , endY2);
+         const id = widget._groups[0][0].parentNode.id; // 获取父元素的id
+        console.log(id);
+     
+         
+         console.log(D3.select(id));
+         console.log(id.slice(7))
+      let delCon =     D3.select(id)
+          .append("g")
+          
+          .attr("id",`${id}-delCon`)
+          .attr('idParent',`${id.slice(7)}`)
+          .attr('cursor','context-menu')
+          .attr('fill','transparent')
+
+          delCon.append("line")
+          .attr('id',111)
+          .attr("x1", `${startX}`)
+          .attr("y1", `${startY}`)
+          .attr("x2", `${endX}`)
+          .attr("y2", `${endY}`)
+          .attr("stroke", `steelblue`)
+          .attr("fill", `transparent`)
+          .attr("stroke-width", `2`)
+          .attr("cursor", `context-menu`)
+     
+          
+
+        delCon
+          .append("line")
+          .attr("id", 222)
+          .attr("x1", `${startX2}`)
+          .attr("y1", `${startY2}`)
+          .attr("x2", `${endX2}`)
+          .attr("y2", `${endY2}`)
+          .attr("stroke", `steelblue`)
+          .attr("stroke-width", `2`)
+          .attr("cursor", `context-menu`)
+          //    .attr('width',`${wh[0]}`)
+          // .attr('height',`${wh[1]}`)
+
+          delCon.on('click',function(e){
+            console.log(e)
+            alert(`发送请求,组件往出派发事件即可,xy点位点: ${
+              xy},宽高: ${wh},id: ${item.preId}`)
+
+          }) */
         })
         .on("end", function (e) {
-          console.log(e);
-
-          const dot = [+e.sourceEvent.offsetX, +e.sourceEvent.offsetY];
-          const id = widget._groups[0][0].parentNode.id
-            ?.replace(/\D/g, "")
-            .replace("-", "");
-          for (let i = 0; i < that.list.length; i++) {
-            let l = that.list[i];
-            if (l.preId == id) {
-              that.list[i].endX =
-                dot[0] - +widget.attr("o-x") + that.list[i].width;
-              that.list[i].endY =
-                dot[1] - +widget.attr("o-y") + that.list[i].height;
-              that.list[i].startX = dot[0] - +widget.attr("o-x");
-              that.list[i].startY = dot[1] - +widget.attr("o-y");
-
-              break;
-            }
-          }
-          console.log(that.list);
-
           widget.attr("fill", color);
           widget = null; // 被拖拽的元素设为空
-          that.init();
         });
     },
-    isNil(val) {
-      return val === null || val === undefined;
+    isNil(val){
+      return val === null || val === undefined
     },
-    getBounding(element) {
-      // 获取dom元素的只读边界
-      if (element instanceof HTMLElement) {
-        return element.getBoundingClientRect();
-      }
-      return {};
-    },
-    create() {
-      this.isDrag = false;
-    },
-    move() {
-      this.isDrag = true;
-    },
+    getBounding(element){ // 获取dom元素的只读边界
+    if (element instanceof HTMLElement) {
+      return element.getBoundingClientRect();
+    }
+    return {};
+  },
   },
 };
 </script>
@@ -915,7 +951,7 @@ body {
   overflow: hidden;
 } */
 .chartWrap {
-  min-height: 100%;
+  height: 100%;
   width: 100%;
   position: relative;
 }
